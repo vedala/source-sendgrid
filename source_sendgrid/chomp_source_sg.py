6f9c42a6926b3d4c9712398e0de6c40734fc04f1
Sundaram Vedala <svedala23@gmail.com>
2018-08-17 09:51:43 -0400

    Add validate_config method to SourceSendgrid

--- source_sendgrid/chomp_source_sg.py
+++ source_sendgrid/chomp_source_sg.py
@@ -13,11 +13,12 @@ class SourceSendgrid(object):
         url_str = construct_url()
         self.items_per_page = ITEMS_PER_PAGE
 
-    def get_batch(self):
-        pass
+    def validate_config(self):
+        if 'top-level-api' not in self.source_config:
+            raise Exception("Key 'top-level-api' is missing")
+        elif self.source_config['top-level-api'] not in ['stats']:
+            raise Exception("Invalid value for key 'top-level-api'")
 
-    def cleanup(self):
-        pass
 
     def construct_url(self):
         start_date = self.source_config['start_date']
@@ -26,32 +27,19 @@ class SourceSendgrid(object):
         sg_url = sg_url + f"&end_date={end_date}"
         return sg_url
 
-    def fetch_and_write_data(extract_name, api_key, fields, start_date,
-                                                end_date, extract_location):
-        filename_with_path = construct_filename(extract_location, extract_name)
-        extract_file = open(filename_with_path, 'w+')
-        csv_writer = csv.writer(extract_file, quotechar='"',
-                                        quoting=csv.QUOTE_NONNUMERIC)
-    
-        sg_url = f"https://api.sendgrid.com/v3/stats?start_date={start_date}"
-        sg_url = sg_url + f"&end_date={end_date}"
+    def build_request(self):
         sg_request =urllib.request.Request(sg_url)
+        sg_request.add_header('Authorization', f"Bearer {api_key}")
+
+    def get_batch(self):
         sg_request.add_header('Authorization', f"Bearer {api_key}")
         sg_response = urllib.request.urlopen(sg_request).read()
         sg_response_decoded = sg_response.decode('utf-8').replace("'", '"')
         sg_response_json = json.loads(sg_response_decoded)
         for row in sg_response_json:
-          write_row_to_file(extract_file, csv_writer, row, fields)
-        extract_file.close()
-    
-    def construct_filename(directory, base_filename):
-        filename_with_path = os.path.join(os.path.expanduser(directory),
-                                                   base_filename + ".csv")
-        return filename_with_path 
-    
-    def write_row_to_file(file_obj, csv_writer, row, fields):
-        field_values_list = []
-        for field in fields:
-            field_values_list.append(row['stats'][0]['metrics'][field])
-        field_values_tuple = tuple(field_values_list)
-        csv_writer.writerow(field_values_tuple)
+            # append to list instead of writing to file
+            # write_row_to_file(extract_file, csv_writer, row, fields)
+            pass
+
+    def cleanup(self):
+        pass
--- tests/test_chomp_source_sg.py
+++ tests/test_chomp_source_sg.py
@@ -19,5 +19,25 @@ class SendgridTestCase(unittest.TestCase):
         expected_url += f"&end_date=2018-07-30"
         self.assertEqual(expected_url, source.construct_url())
 
+
+    @patch.object(SourceSendgrid, '__init__')
+    def test_validate_config(self, mock_init):
+
+        mock_init.return_value = None
+        source = SourceSendgrid()
+
+        source.source_config = {}
+        with self.assertRaises(Exception) as ctx:
+            source.validate_config()
+        self.assertEqual("Key 'top-level-api' is missing", str(ctx.exception))
+
+        source.source_config = {'top-level-api': 'some-invalid-value'}
+        with self.assertRaises(Exception) as ctx:
+            source.validate_config()
+        self.assertEqual("Invalid value for key 'top-level-api'", str(ctx.exception))
+
+        source.source_config = {'top-level-api': 'stats'}
+        self.assertEqual(None, source.validate_config())
+
 if __name__ == "__main__":
     unittest.main()
